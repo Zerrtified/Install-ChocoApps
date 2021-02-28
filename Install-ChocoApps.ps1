1136b8fa3658522f6365fb55101d4fc2c9100609
Steve Zerr <17504275+Zerrtified@users.noreply.github.com>
2021-02-27 23:54:02 -0500

    Update Install-ChocoApps.ps1

--- Install-ChocoApps.ps1
+++ Install-ChocoApps.ps1
@@ -1,44 +1,48 @@
-﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+{
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
 # Installs or upgrades Chocolately to the most current version
 Invoke-Expression ((New-Object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$ChocoPackages = @("notepadplusplus.install",
+$ChocoPackages = @(
+    "GoogleChrome",
+    "notepadplusplus.install",
     "7zip.install",
-    "sysinternals",
-    "git.install",
     "vscode",
-    "azurepowershell",
-    "putty.install",
-    "cygwin",
-    "openssh",
-    "rdcman",
-    "rsat",
-    "spacesniffer",
-    "DotNet4.5.2",
-    "jre8",
-    "wsl",
-    "docker-for-windows",
-    "python",
-    "awscli",
     "f.lux.install",
-    "powertoys",
-    "vim",
-    "neovim",
-    "microsoft-edge",
-    "authy-desktop",
-    "signal",
-    "telegram.install",
+    "steam",
     "WhatsApp",
-    "azurepowershell"
+    "telegram.install",
+    "signal",
+    "discord.install",
+    "logitechgaming",
+    "Firefox",
+    "vlc",
+    "twitch",
+    "adobereader",
+    "Office365ProPlus",
+    "adobedigitaleditions",
+    "handbrake.install",
+    "adobe-creative-cloud",
+    "origin",
+    "tuxguitar",
+    "rdcman",
+    "intel-xtu",
+    "MakeMKV",
+    "bitwarden",
+    "goggalaxy",
+    "battle.net",
+    "powertoys",
+    "epicgameslauncher",
+    "authy-desktop"
 )
 
 foreach ($Package in $ChocoPackages) {
-    try {
-        Start-Process choco -ArgumentList "install $Package -y" -Wait -NoNewWindow
-    }
-    catch {
-        throw "Failed to install $Package"
-    }
+	try {
+		Start-Process choco -ArgumentList "install $Package -y" -Wait -NoNewWindow
+	}
+	catch {
+		throw "Failed to install $Package"
+	}
 }
